e401e72320dbb1fce90bf7a11f4504fcf74b0c8d
Salih <Salih@DESKTOP-9G48RKD>
2021-01-20 01:14:07 +0300

    Gun 3 metot odevi

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri Ekle)
+        {
+            Console.WriteLine("************************************************************************");
+            Console.WriteLine("***Müşteri Eklenmiştir!*** No:" + Ekle.Id + " Ad Soyad:" + Ekle.Adi + " " + Ekle.Soyadi+ " İkametgah Adresi:" +Ekle.Ikametgah);
+            Console.WriteLine("************************************************************************");
+        }
+        public void Sil(Musteri Sil)
+        {
+            Console.WriteLine("************************************************************************");
+            Console.WriteLine("***Bilgileri Yazılan Müşteri Silinmiştir!*** "+Sil.Id+" "+Sil.Adi+" "+Sil.Soyadi+" "+Sil.Ikametgah);
+            Console.WriteLine("************************************************************************");
+        }
+        public void Listele(Musteri listele)
+        {
+            Console.WriteLine("****************************");
+            Console.WriteLine(listele.Id);
+            Console.WriteLine(listele.Adi);
+            Console.WriteLine(listele.Soyadi);
+            Console.WriteLine(listele.Ikametgah);
+            Console.WriteLine("****************************");
+        }
+    }
+}
--- ClassMetotDemo/Program.cs
+++ ClassMetotDemo/Program.cs
@@ -6,7 +6,47 @@ namespace ClassMetotDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Musteri Musteri1 = new Musteri();
+            Musteri1.Id = 01;
+            Musteri1.Adi = "Melek";
+            Musteri1.Soyadi = "Melek";
+            Musteri1.Ikametgah = "Ankara";
+
+            Musteri Musteri2 = new Musteri();
+            Musteri2.Id = 02;
+            Musteri2.Adi = "Salih";
+            Musteri2.Soyadi = "Gül";
+            Musteri2.Ikametgah = "İstanbul";
+
+            Musteri Musteri3 = new Musteri();
+            Musteri3.Id = 03;
+            Musteri3.Adi = "Polat";
+            Musteri3.Soyadi = "Alemdar";
+            Musteri3.Ikametgah = "Almanya";
+
+
+            MusteriManager mManager = new MusteriManager();
+            mManager.Ekle(Musteri1);
+            mManager.Sil(Musteri1);
+            mManager.Listele(Musteri1);
+            mManager.Listele(Musteri2);
+            mManager.Listele(Musteri3);
+
+
+            //Seçilen müşterileri foreach döngüsü ile konsol ekranına yazdıran Kod 
+
+            /*
+            Musteri[] musteriler = new Musteri[] { Musteri1, Musteri2 };
+            foreach (var manager in musteriler)
+            {
+                Console.WriteLine(manager.Id);
+                Console.WriteLine(manager.Adi);
+                Console.WriteLine(manager.Soyadi);
+                Console.WriteLine(manager.Ikametgah);
+                Console.WriteLine("-----------------");
+            }
+            */
+            Console.ReadKey();
         }
     }
 }
